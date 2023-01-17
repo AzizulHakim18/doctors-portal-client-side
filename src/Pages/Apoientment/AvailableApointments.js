93eceee799b41c1b5314284d0d288eb26fe94c70
AzizulHakim18 <hakimrony18@gamil.com>
2023-01-18 01:49:34 +0600

    server data get and post

--- src/Pages/Apoientment/AvailableApointments.js
+++ src/Pages/Apoientment/AvailableApointments.js
@@ -1,20 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { format } from 'date-fns';
 import ApoientmentOptions from './ApoientmentOptions';
 import BookingModal from './BookingModal/BookingModal';
+import { useQuery } from '@tanstack/react-query';
+
 
 
 const AvailableApointments = ({ selectedDate }) => {
 
-
-    const [apointmentOptions, setApointmentOptions] = useState([]);
     const [treatment, setTreatment] = useState(null);
-    console.log(treatment)
-    useEffect(() => {
-        fetch('http://localhost:5000/appointmentOptions')
-            .then(res => res.json())
-            .then(data => setApointmentOptions(data))
-    }, [])
+    console.log(treatment);
+    const date = format(selectedDate, 'PP');
+
+
+    const { data: apointmentOptions = [], refetch } = useQuery({
+        queryKey: ['appointmentoptions', date],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/appointmentoptions?date=${date}`);
+            const data = await res.json();
+            return data
+        }
+    })
+
+    // useEffect(() => {
+    //     fetch('http://localhost:5000/appointmentoptions')
+    //         .then(res => res.json())
+    //         .then(data => setApointmentOptions(data))
+    // }, [])
     return (
         <div className='lg:m-20 md:m-10 sm:m-8 lg:p-20 md:p-8 sm:p-4'>
             <p className='text-2xl font-bold text-emerald-500 text-center my-8 py-4 '>Available Services on {format(selectedDate, 'PP')}</p>
@@ -35,6 +47,7 @@ const AvailableApointments = ({ selectedDate }) => {
                     selectedDate={selectedDate}
                     treatment={treatment}
                     setTreatment={setTreatment}
+                    refetch={refetch}
                 ></BookingModal>
             }
         </div>
--- src/Pages/Apoientment/BookingModal/BookingModal.js
+++ src/Pages/Apoientment/BookingModal/BookingModal.js
@@ -1,15 +1,22 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useContext } from 'react';
+import { toast } from 'react-hot-toast';
+import { AuthContext } from '../../../Context/UserContexts';
 
-const BookingModal = ({ treatment, selectedDate, setTreatment }) => {
+const BookingModal = ({ treatment, selectedDate, setTreatment, refetch }) => {
     const { name, slots } = treatment;
     const date = format(selectedDate, 'PP');
     console.log(name)
 
+    const { user } = useContext(AuthContext);
+    const PersonName = user?.displayName;
+    const personEmail = user?.email;
+
+
     const handleBooking = event => {
         event.preventDefault();
         const form = event.target;
-        const name = form.name.value;
+        const PersonName = form.name.value;
         const email = form.email.value;
         const slot = form.slot.value;
         const phone = form.phone.value;
@@ -18,14 +25,30 @@ const BookingModal = ({ treatment, selectedDate, setTreatment }) => {
         const booking = {
             appointmentDate: date,
             treatment: name,
-            patient: name,
+            patient: PersonName,
             slot,
             email,
             phone
         }
 
+        fetch('http://localhost:5000/bookings', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setTreatment(null)
+                toast('Good Job! successfully Booked', {
+                    icon: '👏',
+                });
+                refetch();
+            })
         console.log(booking);
-        setTreatment(null)
+
     }
     return (
         <div>
@@ -45,8 +68,8 @@ const BookingModal = ({ treatment, selectedDate, setTreatment }) => {
                                 >{slot}</option>)
                             }
                         </select>
-                        <input name='name' type="text" placeholder="Full Name" className="input w-full max-w-xs input-bordered" />
-                        <input name='email' type="email" placeholder="Email" className="input w-full max-w-xs input-bordered" />
+                        <input name='name' type="text" placeholder="Full Name" value={PersonName} className="input w-full max-w-xs input-bordered" />
+                        <input name='email' type="email" placeholder="Email" value={personEmail} className="input w-full max-w-xs input-bordered" />
                         <input name='phone' type="number" placeholder="Phone Number" className="input w-full max-w-xs input-bordered" />
                         <br />
                         <input type="submit" value="Submit" className='w-full text-xl font-bold bg-gray-500 text-white rounded-lg m-2 p-2' />
